d00749b93784882369c089e8a07d95aeb918d6fc
ian stewart <ian.stewart@gmail.com>
2024-03-02 20:53:34 +0000

    change to server.js

--- server.js
+++ server.js
@@ -201,7 +201,7 @@ app.post("/chat", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
